963db506d2d9ef171c1514faffc123ad2d567aae
Falana, Malik <malikfalana@icloud.com>
2022-09-12 11:59:09 -0400

    Initial Commit

---
+++ What's That Color?/What_s_That_Color_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_s_That_Color_App.swift
+//  What's That Color?
+//
+//  Created by Malik Falana on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct What_s_That_Color_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
